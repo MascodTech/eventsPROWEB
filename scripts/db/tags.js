a48b250857314e1f41f01fa114ee11a7980fccc0
Alexander <alexander>
2021-07-16 15:07:26 +0500

    Update

---
+++ scripts/db/tags.js
@@ -0,0 +1,26 @@
+const tags = [
+  {
+    linkName: "Все события",
+    tagName: "all",
+  },
+  {
+    linkName: "Скидки",
+    tagName: "sales",
+  },
+  {
+    linkName: "Специальные предложения",
+    tagName: "special-offers",
+  },
+  {
+    linkName: "Открытые уроки",
+    tagName: "open-lessons",
+  },
+  {
+    linkName: "Призы и подарки",
+    tagName: "gifts",
+  },
+  {
+    linkName: "Другое",
+    tagName: "more",
+  },
+];
